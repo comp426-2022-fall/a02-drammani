2adb03eedc4de2ec548eae32e98253d874cca802
drammani <drammani@live.unc.edu>
2022-10-07 12:56:42 -0400

    Added support for -j

--- cli.js
+++ cli.js
@@ -7,12 +7,12 @@ var argv = minimist(process.argv.slice(2))
 
 if(argv.h) {
     console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE")
-    console.log("   -h          Show this help message and exit")
-    console.log("   -n, -s      Latitude: N positive; S negative")
-    console.log("   -e, -w      Longitude: E positive; W negative")
-    console.log("   -z          Time zone: uses tz.guess() from moment-timezone by default")
-    console.log("   -d 0-6      Day to retieve weather: 0 is today; defaults to 1")
-    console.log("   -j          Echo pretty JSON from open-meteo API and exit")
+    console.log("   -h          Show this help message and exit.")
+    console.log("   -n, -s      Latitude: N positive; S negative.")
+    console.log("   -e, -w      Longitude: E positive; W negative.")
+    console.log("   -z          Time zone: uses tz.guess() from moment-timezone by default.")
+    console.log("   -d 0-6      Day to retieve weather: 0 is today; defaults to 1.")
+    console.log("   -j          Echo pretty JSON from open-meteo API and exit.")
     process.exit(0)
 }
 
@@ -23,6 +23,11 @@ const longitude = argv.e || -1*argv.w
 
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&daily=precipitation_hours&timezone='+timezone)
 const data = await response.json()
+
+if(argv.j) {
+    console.log(data)
+    process.exit(0)
+}
 
 const days = argv.d
 
@@ -39,3 +44,5 @@ if(days == 0) {
 } else {
     console.log("tomorrow.")
 }
+
+
